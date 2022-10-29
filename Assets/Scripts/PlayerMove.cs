a262c230ef2f28a08efe2854e52c310e2b0b03e7
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2022-10-29 19:50:43 -0300

    movimento consertado

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,54 +4,29 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public CharacterController charController; // Publicly define our character controller
+    public CharacterController charController;
 
-    public bool canJump; // Enable this in the inspector if you want the player to jump
-
-    public float walkSpeed = 2.5f;
-    public float runSpeed = 5f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 1f;
-    public Transform groundCheck; // The transform of our "Ground Check" object. Used to determine if we are on the ground
-    public float groundDistance = 0.05f;
-    public LayerMask groundMask; // Set a field for our "Ground" layer
-    float speed; // Update-able moving speed
-
+    public float gravity,speed,jumpHeight;
+    public Transform groundCheck; 
+    public LayerMask groundMask; 
     Vector3 velocity;
-    public bool grounded; // Defines is the player is touching the ground
+    public bool grounded; 
 
-    private void Update() // Called once per frame
+    private void Update()
     {
-        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // Checks if our player is touching an object with a "Ground" layer and sets the boolean accordingly
+        grounded = Physics.CheckSphere(groundCheck.position, 0.05f, groundMask);
 
-        if (grounded && velocity.y < 0) // If the player is grounded
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical"); 
+
+        charController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && grounded) 
         {
-            velocity.y = -2f; // Set the downward velocity to 2
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
         }
 
-        if (Input.GetKey(KeyCode.LeftShift)) // If the left shift button is being pressed
-        {
-            speed = runSpeed; // Set our speed to runSpeed
-        }
-        else
-        {
-            speed = walkSpeed;// Set our speed to walkSpeed
-        }
+        velocity.y += gravity * Time.deltaTime;
 
-        float x = Input.GetAxis("Horizontal"); // Define an X variable and set it to Unity's "Horizontal" axis
-        float z = Input.GetAxis("Vertical"); // Define a Y variable and set it to Unity's "Vertical" axis
-
-        Vector3 move = transform.right * x + transform.forward * z; // Define our move vector
-
-        charController.Move(move * speed * Time.deltaTime); // Move our character controller based on our set inputs
-
-        if (Input.GetButtonDown("Jump") && grounded && canJump) // Unity's default input for "Jump" is the space key
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // Set our jumping gravity
-        }
-
-        velocity.y += gravity * Time.deltaTime; // Set our regular gravity
-
-        charController.Move(velocity * Time.deltaTime); // Apply our gravity to our character controller
+        charController.Move(velocity * Time.deltaTime); 
     }
 }
